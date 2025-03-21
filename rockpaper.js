4841ef4b50c108daf077da72579326a4e0c3d62a
Dimitris <57366051+DimitrisBoutakoglou@users.noreply.github.com>
2025-03-21 15:57:43 +0200

    Edited comments placement and added user choice function. Not yet fully functional

--- rockpaper.js
+++ rockpaper.js
@@ -1,11 +1,23 @@
 
+//Ask user to type rock paper or scissors
+function userChoice(rock, paper, scissors){
+    let choice = prompt("Choose Rock, Paper or Scissors: ");
+    return choice;
+}
+console.log(userChoice());
+
+
+/*
+//Present user's choise in the console
+console.log(userChoice());
+
 //Make a random number generator
 //Assign rock-paper-scissors to the numbers
-
 function computerChoice(){
     let randomNumber = Math.random()*100+1;
 
     if(randomNumber <= 33.3333333333){
+        let rock = randomNumber
         console.log('Rock');
     }
     else if(randomNumber > 33.3333333333 && randomNumber < 66.6666666666){
@@ -13,10 +25,17 @@ function computerChoice(){
     }
     else console.log('Scissors');
 }
+
+//Present the generator's choise in the console
 console.log(computerChoice());
 
+function winState(){
+    if((userChoice = "Rock") && (randomNumber = rock));
+    console.log('tie');
+}
+*/
 
-//Ask user to type rock paper or scissors
-//Present user's choise in the console
-//Present the generator's choise in the console
+
+
+
 //Announce the winner
