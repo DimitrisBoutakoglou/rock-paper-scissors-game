d2366f285fa30391823c4576ef7d7b67183fd6f4
Dimitris <57366051+DimitrisBoutakoglou@users.noreply.github.com>
2025-03-24 15:50:39 +0200

    Overhauled js. Now user input and computer input are stored and compared correctly.

--- rockpaper.js
+++ rockpaper.js
@@ -1,50 +1,60 @@
 
-//Ask user to type rock paper or scissors
-let rock;
-let paper;
-let scissors;
-
-function userChoice(){
-
-    let input = prompt("Chooce rock, paper, or scissors: ");
-    if(input === "rock"){
-        let rock = "rock";
-        console.log(rock);
-    }
-    else if(input === "paper"){
-        let paper = "paper";
-        console.log(paper);
-    }
-    else if(input === "scissors"){
-        let scissors = "scissors";
-        console.log(scissors);
-    }
-    else console.log("Please choose between rock, paper or scissors!")
-}
-userChoice();
-
-//Make a random number generator
-//Assign rock-paper-scissors to the numbers
-//Present the generator's choise in the console
 function computerChoice(){
-    let randomNumber = Math.random()*100+1;
-
-    if(randomNumber <= 33.3333333333){
-        let rock = randomNumber;
-        console.log("Rock");
-        return rock;
+    let randomNumber = Math.floor(Math.random() * 3) + 1; //epistrefei random arithmo apo 1-3
+    if(randomNumber == 1){
+        console.log("Computer chose rock");
+        return "rock";
+    }  
+    else if(randomNumber == 2){
+        console.log("Computer chose paper");
+        return "paper";
     }
-    else if(randomNumber > 33.3333333333 && randomNumber < 66.6666666666){
-        let paper = randomNumber;
-        console.log("Paper");
-        return paper;
+    else if(randomNumber == 3){
+        console.log("Computer chose scissors");
+        return "scissors";
     }
-    else{ 
-        let randomNumber = scissors;
-        console.log('Scissors');
-        return scissors;
-        }
 }
-computerChoice();
 
-//Announce the winner
+
+
+function getHumanChoice(){
+    let humanChoice = prompt("Choose: ")
+    if(humanChoice === "rock"){
+        console.log("You chose rock");
+        return "rock";
+    }
+    else if(humanChoice === "paper"){
+        console.log("You chose paper");
+        return "paper";
+    }
+    else if(humanChoice === "scissors"){
+        console.log("You chose scissors");
+        return "scissors";
+    }
+}
+
+function result(computerChoice, getHumanChoice){
+    if (computerChoice === getHumanChoice){
+        return "Tie!";
+    }
+    if (computerChoice=== "rock" && getHumanChoice === "paper"){
+        return "You win! Paper beats rock!";
+    }
+    if(computerChoice === "rock" && getHumanChoice === "scissors"){
+        return "You lose! Rock beats scissors!";
+    }
+    if(computerChoice === "paper" && getHumanChoice === "rock"){
+        return "You lose! Paper beats rock!";
+    }
+    if(computerChoice === "paper" && getHumanChoice === "scissors"){
+        return "You win! Scissors beats paper!";
+    }
+    if(computerChoice === "scissors" && getHumanChoice === "paper"){
+        return "You lose! Scissors beats paper!";
+    }
+    if (computerChoice === "scissors" && getHumanChoice === "rock"){
+        return "You win! Rock beats scissors!";
+    }
+}
+
+console.log(result (computerChoice(), getHumanChoice()));
